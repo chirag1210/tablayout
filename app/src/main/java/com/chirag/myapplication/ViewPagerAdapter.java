04a262e15d4eecd84cb86d3e75ea076c6a24be2a
chirag1210 <chirag.chaudhari7@gmil.com>
2019-03-17 19:37:21 +0530

    Add all my files

---
+++ app/src/main/java/com/chirag/myapplication/ViewPagerAdapter.java
@@ -0,0 +1,29 @@
+package com.chirag.myapplication;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private String title[] = {"Credit card", "Debit card", "PayPal","UPI"};
+
+    public ViewPagerAdapter(FragmentManager manager) {
+        super(manager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return TabFragment.getInstance(position);
+    }
+
+    @Override
+    public int getCount() {
+        return title.length;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return title[position];
+    }
+}
